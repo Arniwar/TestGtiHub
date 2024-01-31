e600739f2bef3ef0f47f6682f5b56274b2107211
Arnau Guerra González <a2308738@institutmontilivi.cat>
2024-01-31 20:32:30 +0100

    update to github

--- TestGtiHub/Program.cs
+++ TestGtiHub/Program.cs
@@ -5,6 +5,7 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, Montilivi!");
+            Console.WriteLine("I'm the student, I'm learning GitHub");
         }
     }
 }
