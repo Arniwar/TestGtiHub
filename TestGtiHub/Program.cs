49075e2e6f850157616a9343dd5349a650c8a7a9
Arniwar <158340602+Arniwar@users.noreply.github.com>
2024-01-31 20:28:08 +0100

    Update Program.cs

--- TestGtiHub/Program.cs
+++ TestGtiHub/Program.cs
@@ -1,10 +1,10 @@
-﻿namespace TestGtiHub
+﻿namespace TestGitHub
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, Montilivi!");
         }
     }
 }
